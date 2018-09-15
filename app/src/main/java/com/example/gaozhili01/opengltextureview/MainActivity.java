d4b599386b613c11bbf655b856bde57add7f28f4
lance <gaozhili01@baidu.com>
2018-09-15 19:40:44 +0800

    add the adapter of width height adaption in video playing scene

--- app/src/main/java/com/example/gaozhili01/opengltextureview/MainActivity.java
+++ app/src/main/java/com/example/gaozhili01/opengltextureview/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.gaozhili01.opengltextureview;
 
 import android.app.Activity;
+import android.graphics.Matrix;
 import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.MenuInflater;
+import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 
 import java.io.IOException;
 
-public class MainActivity extends Activity implements TextureView.SurfaceTextureListener, MediaPlayer.OnPreparedListener {
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnVideoSizeChangedListener {
 
     private String videoPath = Environment.getExternalStorageDirectory().getPath() + "/testFace.mp4";
     private TextureView textureView;
     private MediaPlayer mediaPlayer;
     private SurfaceTexture surfaceTexture;
     private Surface surface;
+    private final static String TAG = "MainActivity";
+    public int mVideoMode = 0;
+    private int mVideoWidth;
+    private int mVideoHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +68,9 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             mediaPlayer.setSurface(surface);
             mediaPlayer.prepareAsync();
             mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setOnErrorListener(this);
+            mediaPlayer.setOnBufferingUpdateListener(this);
+            mediaPlayer.setOnVideoSizeChangedListener(this);
             mediaPlayer.setLooping(true);
         } catch (IOException e) {
             e.printStackTrace();
@@ -76,7 +84,7 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
 
     @Override
     public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
+        updateTextureViewSizeCenter();
     }
 
     @Override
@@ -92,7 +100,40 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
         if (mediaPlayer != null) {
+            mediaPlayer.setVolume(1f, 1f);
             mediaPlayer.start();
         }
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+        Log.i(TAG, "Buffering: " + i);
+    }
+
+    @Override
+    public void onVideoSizeChanged(MediaPlayer mediaPlayer, int i, int i1) {
+        mVideoWidth = mediaPlayer.getVideoWidth();
+        mVideoHeight = mediaPlayer.getVideoHeight();
+        updateTextureViewSizeCenter();
+    }
+
+    private void updateTextureViewSizeCenter() {
+        float sx = (float)textureView.getWidth() / (float)mVideoWidth;
+        float sy = (float)textureView.getHeight() / (float)mVideoHeight;
+        Matrix matrix = new Matrix();
+        matrix.preTranslate((textureView.getWidth() - mVideoWidth) / 2, (textureView.getHeight() - mVideoHeight) / 2);
+        matrix.preScale(mVideoWidth / (float)textureView.getWidth(), mVideoHeight / (float)textureView.getHeight());
+        if (sx >= sy) {
+            matrix.postScale(sy, sy, textureView.getWidth() / 2, textureView.getHeight() / 2);
+        } else {
+            matrix.postScale(sx, sx, textureView.getWidth() / 2, textureView.getHeight() / 2);
+        }
+        textureView.setTransform(matrix);
+        textureView.postInvalidate();
     }
 }
